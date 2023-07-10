5612ada5b7c9e906106671e06803ae0e69d8e356
Diego Isaac Hurtado Miguel <108156269+diegoisaacpy@users.noreply.github.com>
2023-07-10 17:26:41 -0600

    Fixed errors

--- ArditSulce/TodoistAPP/gui.py
+++ ArditSulce/TodoistAPP/gui.py
@@ -1,5 +1,10 @@
 import PySimpleGUI as sg
 import functions
+import time
+
+sg.theme("Black")
+
+clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do: ")
 input_box = sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = sg.Button("Add")
@@ -10,15 +15,15 @@ complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
                    font=("Helvetica", 20))
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
+    event, values = window.read(timeout=10)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -26,27 +31,32 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
         case "Edit":
-            todo_to_edit = values["todos"][0]
-            new_todo = values["todo"]
+            try:
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
+                todo_to_edit = values["todos"][0]
+                new_todo = values["todo"]
+
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+            except IndexError:
+                sg.popup("There is an error")
 
         case "Complete":
-            todo_to_complete = values["todos"][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value="")
+            try:
+                todo_to_complete = values["todos"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value="")
+            except IndexError:
+                sg.popup("There is an error")
+
         case "Exit":
             break
         case sg.WIN_CLOSED:
             break
-
-
-
 window.close()
